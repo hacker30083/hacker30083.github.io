a71df5c25c321bf47fc2af082313777ad31298ee
Kaspar Aun <hacker30083@hotmail.com>
2026-07-25 17:07:04 +0300

    Added new project and fixed styling issues.

--- footer.js
+++ footer.js
@@ -2,4 +2,4 @@ let year = new Date().getFullYear();
 
 const copyrightText = document.getElementById("copyright-text");
 
-copyrightText.textContent = "Copyright " + year + " Kaspar Aun"
+copyrightText.textContent = "Copyright © " + year + " Kaspar Aun"
